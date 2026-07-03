a991dcd43bab3df44a982471aea4ff9dc8c8122f
Идиакант <kushner.maksim.v@gmail.com>
2026-07-03 11:45:23 +0200

    Update config.php

--- config.php
+++ config.php
@@ -9,4 +9,4 @@ define('DB_PASSWORD', 'kushnermaks12');
 // Данные для отправки картинок в Supabase Storage
 define('SUPABASE_PROJECT_ID', 'tszlvntdykvzrzavckea');
 define('SUPABASE_ANON_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzemx2bnRkeWt2enJ6YXZja2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NjI4NzgsImV4cCI6MjA5ODUzODg3OH0.smGi6sXveyvoLvBK6UhuudV9eik42hWkMT90Cu9N5kA');
-define('SUPABASE_BUCKET', 'images'); // Убедись, что бакет называется images и он public!
+define('SUPABASE_BUCKET', 'my-4ch'); // Убедись, что бакет называется images и он public!
