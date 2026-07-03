ef6e4a819ffc8e962914a54452fe51ae4a5f8735
Идиакант <kushner.maksim.v@gmail.com>
2026-07-03 11:49:17 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,18 @@
 <?php
-// Твои настройки подключения к базе данных Supabase
+// Твой ID проекта из Supabase
+$project_id = 'tszlvntdykvzrzavckea'; 
+
+// Твой длинный API-ключ anon public
+$anon_key = 'EyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzemx2bnRkeWt2enJ6YXZja2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NjI4NzgsImV4cCI6MjA5ODUzODg3OH0.smGi6sXveyvoLvBK6UhuudV9eik42hWkMT90Cu9N5kA';
+
+
+// НАСТРОЙКИ (Код сам всё подставит автоматически!)
 define('DB_HOST', 'aws-0-eu-central-1.pooler.supabase.com');
 define('DB_PORT', 6543);
 define('DB_NAME', 'postgres');
-define('DB_USER', 'postgres.db');
+define('DB_USER', 'postgres.' . $project_id);
 define('DB_PASSWORD', 'kushnermaks12');
 
-// Данные для отправки картинок в Supabase Storage
-define('SUPABASE_PROJECT_ID', 'tszlvntdykvzrzavckea');
-define('SUPABASE_ANON_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzemx2bnRkeWt2enJ6YXZja2VhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI5NjI4NzgsImV4cCI6MjA5ODUzODg3OH0.smGi6sXveyvoLvBK6UhuudV9eik42hWkMT90Cu9N5kA');
-define('SUPABASE_BUCKET', 'my-4ch'); // Убедись, что бакет называется images и он public!
+define('SUPABASE_PROJECT_ID', $project_id);
+define('SUPABASE_ANON_KEY', $anon_key);
+define('SUPABASE_BUCKET', 'my-4ch'); // Название твоего бакета
