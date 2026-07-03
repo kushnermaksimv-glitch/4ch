160fcea610eea9cc9d4278fc8e6df4a74699e6fa
Идиакант <kushner.maksim.v@gmail.com>
2026-07-03 11:17:15 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+// Твои настройки подключения к базе данных Supabase
+define('DB_HOST', 'aws-0-eu-central-1.pooler.supabase.com');
+define('DB_PORT', 6543);
+define('DB_NAME', 'postgres');
+define('DB_USER', 'postgres.db');
+define('DB_PASSWORD', 'kushnermaks12');
+
+// Данные для отправки картинок в Supabase Storage
+define('SUPABASE_PROJECT_ID', 'db');
+define('SUPABASE_ANON_KEY', 'СЮДА_ВСТАВЬ_СВОЙ_ДЛИННЫЙ_ANON_KEY');
+define('SUPABASE_BUCKET', 'images'); // Убедись, что бакет называется images и он public!
