38496cc3b6d9ebf8b6ffa0a53e27e7821363403d
Идиакант <kushner.maksim.v@gmail.com>
2026-07-03 11:52:47 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,12 +1,16 @@
 <?php
-require_once 'config.php';
 header('Content-Type: application/json');
+
+define('DB_HOST', 'aws-0-eu-central-1.pooler.supabase.com');
+define('DB_PORT', 6543);
+define('DB_NAME', 'postgres');
+define('DB_USER', 'postgres.tszlvntdykvzrzavckea');
+define('DB_PASSWORD', 'kushnermaks12');
 
 try {
     $dsn = "pgsql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME;
     $pdo = new PDO($dsn, DB_USER, DB_PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
-    // Проверяем существование таблицы
     $pdo->exec("CREATE TABLE IF NOT EXISTS custom_posts (
         id SERIAL PRIMARY KEY,
         name VARCHAR(100),
